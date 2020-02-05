e069850808e9de29f341fc129e56f62fccc4dbc7
osmel <odfarak@gmail.com>
2020-02-05 22:29:12 +0000

    new new new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,17 +6,15 @@
  */
 int main(void)
 {
-
-  char chavar;
-  int invar;
-  long linvar;
-  double l2invar;
-  float flovar;
-
+char chavar;
+int invar;
+long linvar;
+double l2invar;
+float flovar;
 printf("Size of a char: %ld byte(s)\n", sizeof(chavar));
 printf("Size of an int: %ld byte(s)\n", sizeof(invar));
 printf("Size of a long int: %ld byte(s)\n", sizeof(linvar));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(l2invar));
-printf("Size of a float: %ld byte(s)\n", sizeof(flovar)); 
+printf("Size of a float: %ld byte(s)\n", sizeof(flovar));
 return (0);
 }
