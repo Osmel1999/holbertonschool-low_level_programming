4ec5ba1dc54854416a54ee4b9a44d237c399ee84
osmel <odfarak@gmail.com>
2020-02-05 21:42:08 +0000

    this yes! i hope

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,17 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(void);
 {
 char chavar;
 int invar;
 long int linvar;
 long long int l2invar;
 float flovar;
-printf("Size of char:  %ld bytes\n", sizeof(chavar));
-printf("Size of int:  %ld bytes\n", sizeof(invar));
-printf("Size of long int:  %ld bytes\n", sizeof(linvar));
-printf("Size of long long int:  %ld bytes\n", sizeof(l2invar));
-printf("Size of float:  %ld bytes\n", sizeof(flovar));
+printf("Size of char:  %ld byte(s)\n", sizeof(chavar));
+printf("Size of int:  %ld byte(s)\n", sizeof(invar));
+printf("Size of long int:  %ld byte(s)\n", sizeof(linvar));
+printf("Size of long long int:  %ld byte(s)\n", sizeof(l2invar));
+printf("Size of float:  %ld byte(s)\n", sizeof(flovar));
 return (0);
 }
