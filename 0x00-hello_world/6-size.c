b709c2359178dcacd0cc508f42fe4337e817737b
osmel <odfarak@gmail.com>
2020-02-05 21:47:11 +0000

    new new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int invar;
 long int linvar;
 long long int l2invar;
 float flovar;
-printf("Size of char:  %ld byte\n", sizeof(chavar));
-printf("Size of int:  %ld bytes\n", sizeof(invar));
-printf("Size of long int:  %ld bytes\n", sizeof(linvar));
-printf("Size of long long int:  %ld bytes\n", sizeof(l2invar));
-printf("Size of float:  %ld bytes\n", sizeof(flovar));
+printf("Size of char: %ld byte\n", sizeof(chavar));
+printf("Size of int: %ld bytes\n", sizeof(invar));
+printf("Size of long int: %ld bytes\n", sizeof(linvar));
+printf("Size of long long int: %ld bytes\n", sizeof(l2invar));
+printf("Size of float: %ld bytes\n", sizeof(flovar));
 return (0);
 }
