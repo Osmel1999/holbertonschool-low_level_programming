7e3a5f4bec0f9cac734b61d75344279c36177269
osmel <odfarak@gmail.com>
2020-02-06 21:49:13 +0000

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; ++i)
+{
+if (i == 'e' || i == 'q')
+{
+++i;
+}
+putchar(i);
+}
+return (0);
+}
