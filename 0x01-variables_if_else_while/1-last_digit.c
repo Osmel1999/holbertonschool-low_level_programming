709961343b2989378e7e328f13bfcbaffce3874f
osmel <odfarak@gmail.com>
2020-02-06 19:43:36 +0000

    newnewnewnewnewnewen

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ if (m == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, m);
 }
-if (m < 6 && m != o)
+if (m < 6 && m != 0)
 {
 printf("Laste digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
