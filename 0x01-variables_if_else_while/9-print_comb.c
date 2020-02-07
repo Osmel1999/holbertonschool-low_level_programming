3199ebafc0a3b1f3cb0b17c195bd6c0ea39822b2
osmel <odfarak@gmail.com>
2020-02-07 19:22:48 +0000

    este si que si

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,8 +4,7 @@
  *
  * Return: Always 0 (Success)
 */
-int main(void)
-{
+int main(void) {
 int i;
 for (i = '0'; i <= '9'; ++i)
 {
@@ -15,7 +14,7 @@ if (i != '9')
 putchar(',');
 putchar(' ');
 }
+}
 putchar('\n');
 return (0);
-}
 }
