a6a6079294a3036a5847266351888023c319def9
osmel <odfarak@gmail.com>
2020-02-07 20:30:58 +0000

    este si que si

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,11 +16,11 @@ while (i < 999)
 r = (i / 100 + '0');
 l = (i % 10 + '0');
 c = (i / 10 % 10 + '0');
-if (r < l && c < l)
+if (l < c && c < r)
 {
-putchar(r);
-putchar(c);
 putchar(l);
+putchar(c);
+putchar(r);
 if (i != 789)
 {
 putchar(',');
