00bb1632bb9dede87cf1e551ea7910243e4a4bd2
osmel <odfarak@gmail.com>
2020-02-11 18:48:48 +0000

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _abs - check the code for Holberton School students.
+ * @a: variable of the function.
+ * Return: Always int.
+ */
+int _abs(int a)
+{
+if (a >= 0)
+{
+return (a);
+}
+return (-a);
+}
