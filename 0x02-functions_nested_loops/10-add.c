14873297bdc73033ad17ce7cc427c441faec7cb0
osmel <odfarak@gmail.com>
2020-02-11 21:57:46 +0000

    new

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ * @a: variable of the parameter
+ * @b: variable of the parameter
+ * Return: And int.
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
