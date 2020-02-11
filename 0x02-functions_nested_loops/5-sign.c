9cdadded68043bf5dbf774128ed381f9fb75aae0
osmel <odfarak@gmail.com>
2020-02-11 18:08:04 +0000

    new

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign - check the code for Holberton School students.
+ * @n: this is the parameter of the function.
+ * Return: 1 if n is greater than 0, 0 if is equal and -1 if is less than 0.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar('+');
+}
+if (n == 0)
+{
+return (0);
+_putchar('0');
+}
+if (n < 0)
+{
+return (-1);
+_putchar('-');
+}
+}
