3111092499915da675fc7d8367afd7f762beb72d
osmel <odfarak@gmail.com>
2020-02-11 19:50:09 +0000

    new

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * jack_bauer - check the code for Holberton School students.
+ *
+ * Return: Nothing 'cause the function is void.
+ */
+void jack_bauer(void)
+{
+int l = 0;
+while (l < 24)
+{
+int r = 0;
+while (r < 60)
+{
+_putchar(l / 10 + '0');
+_putchar(l % 10 + '0');
+_putchar('\n');
+_putchar(r / 10 + '0');
+_putchar(r % 10 + '0');
+r++;
+}
+l++;
+}
+}
