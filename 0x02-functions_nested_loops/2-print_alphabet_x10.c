f4cc323a281340484e342990c5d8811e266514a5
osmel <odfarak@gmail.com>
2020-02-11 14:52:26 +0000

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
 int c;
 char A;
-for (A = 'a'; )
+for (c = 0; c < 10; c++)
+{
+for (A = 'a'; A <= 'z'; A++)
+{
+_putchar(A);
+}
+_putchar('\n');
+}
+}
