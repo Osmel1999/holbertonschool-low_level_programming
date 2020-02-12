3dcd1c6baea23aaf4f6340492232f8481596c181
osmel <odfarakm@gmail.com>
2020-02-11 22:41:14 -0500

    new

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+include "holberton.h"
+/**
+ * main - this function print the int positive and once that are  * negative 
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
