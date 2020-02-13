620b52bb9125c13ae2d62b4dab69c2122f32a2e1
osmel <odfarakm@gmail.com>
2020-02-13 17:08:29 -0500

    new

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int c; 
+
+for (c = 1; c <= 100; c++)
+	printf("%d", c);
+}
