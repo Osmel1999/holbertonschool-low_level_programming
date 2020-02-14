333e2b829dd3babfc7c0d468a125aa752d2d2c8a
osmel <odfarakm@gmail.com>
2020-02-13 21:18:10 -0500

    new 3

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- *print_triangle - Print numeral signe n time
- *@size: Parameter
- *Return: always return zero
+ *print_triangle - check the code for Holberton School students.
+ *@size: variable of the function
+ *Return: always 0.
  */
 
 void print_triangle(int size)
