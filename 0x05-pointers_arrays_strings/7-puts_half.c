536ce6af827d1c8efe6f1ca50539d50a33757e70
osmel <odfarakm@gmail.com>
2020-02-18 17:41:31 -0500

    new

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,22 @@
 void puts_half(char *str)
 {
 int c;
+int n;
 char p;
 for (c = 0; str[c] != '\0'; c++)
 {
-if (c >= 5)
-{
-p = str[c];
-_putchar(p);
 }
+if (c % 2 == 0)
+{
+n = c / 2;
+}
+else
+{
+n = (c + 1) / 2;
+}
+for (; n < c; n++)
+{
+_putchar(str[n]);
 }
 _putchar('\n');
 }
