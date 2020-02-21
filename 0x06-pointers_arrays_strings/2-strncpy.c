36c05a67a5b126ec842513183ceec66b6f0630c0
Osmel1999 <60365283+Osmel1999@users.noreply.github.com>
2020-02-20 23:01:43 -0500

    new

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,21 @@
-
+#include "holberton.h"
+/**
+ * _strchr - 
+ * character c in the string s
+ * @c: variable of the function
+ * @s: pointer of the function
+ * Return: pointer to first occurence of character c or null if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+	if (*s == c)
+	return (s);
+	s++;
+	}
+	if (*s == c)
+	return (s);
+	else
+	return ('\0');
+}
