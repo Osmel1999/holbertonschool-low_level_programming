ab8eb39197c5c779e796610ea5afeb44e543828e
Osmel1999 <60365283+Osmel1999@users.noreply.github.com>
2020-02-20 23:10:05 -0500

    new

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,16 +6,17 @@
  * @s: pointer of the function
  * Return: pointer to first occurence of character c or null if not found
  */
-char *_strchr(char *s, char c)
+char *_strncpy(char *dest, char *src, int n)
 {
-	while (*s != '\0')
-	{
-	if (*s == c)
-	return (s);
-	s++;
-	}
-	if (*s == c)
-	return (s);
-	else
-	return ('\0');
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for ( ; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
