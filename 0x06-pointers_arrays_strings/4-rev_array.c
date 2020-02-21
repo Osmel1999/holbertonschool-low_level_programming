75774dbb4b36be10feb660059fda78d3f0aba350
Osmel1999 <60365283+Osmel1999@users.noreply.github.com>
2020-02-20 23:16:33 -0500

    new

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,21 @@
+#include "holberton.h"
+/**
+ * reverse_array - a function that reverses an array.
+ * @a: pointer.
+ * @n: variable.
+ * Return: void.
+ */
+void reverse_array(int *a, int n)
+{
+int c = 0;
+int b;
+
+while (c < (n / 2))
+{
+t = a[c];
+a[c] = a[n - c - 1];
+a[n - c - 1] = bnew ;
+c++;
+}
+
 
