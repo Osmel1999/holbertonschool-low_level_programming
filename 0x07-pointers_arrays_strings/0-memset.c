00dcdd8f756f9ac65f96e38048e2b568a6ed5442
osmel <odfarakm@gmail.com>
2020-02-25 11:22:25 -0500

    new2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,10 @@
 #include "holberton.h"
 /**
-<<<<<<< HEAD
- * _memset - Fills memory with a constant byte
- * @s: Array of char
- * @b: Constant character
- * @n: turn: Array filled with the constant
-=======
  * _memset - check the code for Holberton School students.
  * @s: Array
  * @b: contans
  * @n: Number of bytes
  * Return: Array 
->>>>>>> b31b3a5bc97d078eb2bcba00f37f38fb7c4fd3f8
  **/
 char *_memset(char *s, char b, unsigned int n)
 {
