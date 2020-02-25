4447ea88c15d7a8a83e9016f8fad6b19cc5155c8
osmel <odfarakm@gmail.com>
2020-02-25 03:36:01 +0000

    new

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,9 @@
-i#include "holberton.h"
-
+#include "holberton.h"
 /**
  * _memset - Fills memory with a constant byte
  * @s: Array of char
  * @b: Constant character
- * @n: Number of bytes
- * Return: Array filled with the constant
+ * @n: turn: Array filled with the constant
  **/
 char *_memset(char *s, char b, unsigned int n)
 {
