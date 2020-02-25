e0f4cf4286b91738db6e3337b75bc1a31ca23682
Osmel1999 <60365283+Osmel1999@users.noreply.github.com>
2020-02-24 23:13:48 -0500

    ofhwofhwp

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
  * _strspn - check the code for Holberton School students.
  * @s:
@@ -10,26 +9,22 @@
  **/
 unsigned int _strspn(char *s, char *accept)
 {
-	int l;
-  int a;
-  int j;
-
-	len = 0;
-	for (a = 0; s[a] != '\0'; a++)
-	{
-		for (b = 0; accept[b] != '\0'; b++)
-		{
-			if (s[a] == accept[b])
-			{
-				l++;
-				break;
-			}
-		}
-
-		if (s[a] != accept[b])
-			break;
-
-	}
-
-	return (l);
+int l;
+int a;
+int j;
+len = 0;
+for (a = 0; s[a] != '\0'; a++)
+{
+for (b = 0; accept[b] != '\0'; b++)
+{
+if (s[a] == accept[b])
+{
+l++;
+break;
+}
+}
+if (s[a] != accept[b])
+break;
+}
+return (l);
 }
