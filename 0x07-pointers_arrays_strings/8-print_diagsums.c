1a0b2d051dd8860e2ff1f6caef439a3a40ba16c5
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-26 15:06:19 -0500

    new

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,26 +1,17 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
- * print_diagsums - check the code for Holberton School students.
- * @a: Array
- * @size: variable
- **/
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
 void print_diagsums(int *a, int size)
 {
-int i, size_x2, sum, times;
-size_x2 = size * size;
-sum = 0;
-for (i = 0; i < size_x2; i += size + 1)
-{
-sum += a[i];
-}
-printf("%d, ", sum);
-sum = 0;
-times = 0;
-for (i = size - 1; i < size_x2; i = size - 1 + i)
-{
-if (times != size)
-sum += a[i];
-times++;
-}
-printf("%d\n", sum);
+int i, n, total1 = 0, total2 = 0;
+for (i = 0; i <= (size * size); i = i + size + 1)
+total1 = total1 + a[i];
+for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+total2 = total2 + a[n];
+printf("%d, %d\n", total1, total2);
 }
