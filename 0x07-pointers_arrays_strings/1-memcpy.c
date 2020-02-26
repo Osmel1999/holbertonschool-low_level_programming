3ba409f4ad18e6dc40514bd25d6b88d995ec2811
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-26 10:53:13 -0500

    fix betty error

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-
 /**
  * *_memcpy - check the code for Holberton School students.
  * @dest: pointer
  * @src: pointer
  * @n: variable
- *
- * Return: pointer to dest
+ * Return: pointer dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
+unsigned int a;
+for (a = 0; a < n; a++)
 {
-dest[i] = src[i];
+dest[a] = src[a];
 }
 return (dest);
 }
