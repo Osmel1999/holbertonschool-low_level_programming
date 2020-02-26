7b25a1e2132bd5ea94f82bece5590a74dc4357fc
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-26 11:03:30 -0500

    new new ne

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
  * _strchr - check the code for Holberton School students.
- * @s: Something
- * @c: something
- * Return: 
- **/
+ * @s: pointer
+ * @c: variable
+ * Return: Always 0.
+ */
 char *_strchr(char *s, char c)
 {
 for (; *s; s++)
-{
+}
 if (*s == c)
+{
 return (s);
 }
 if (*s == c)
