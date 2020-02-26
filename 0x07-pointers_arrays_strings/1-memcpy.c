c83ab05209872c1316e266af077bdf3e567cc11c
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-26 10:34:54 -0500

    new new new

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,19 @@
 #include "holberton.h"
+
 /**
- * _memcpy - check the code for Holberton School students.
- * @dest: pointer Destination 
+ * *_memcpy - check the code for Holberton School students.
+ * @dest: pointer
  * @src: pointer
  * @n: variable
- * Return: 
- **/
+ *
+ * Return: pointer to dest
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int a;
-for (a = 0; a < n; a++)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
-dest[a] = src[a];
+dest[i] = src[i];
 }
 return (dest);
 }
