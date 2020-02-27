2cfceb4770fbfb8e61677e68e95d180e1e8d197e
osmel <odfarakm@gmail.com>
2020-02-26 20:37:55 -0500

    newnew

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,28 @@
 #include "holberton.h"
+#include <stdio.h>
+
 /**
- * _strchr - check the code for Holberton School students.
- * @s: pointer
- * @c: variable
- * Return: Always 0.
+ * *_strchr - locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ *
+ * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-int a;
+		int a;
 
-for (a = 0; s[a] >= '\0'; a++)
-{
-if (s[a] == c)
-{
-return (s + a);
-}
-}
-return (NULL);
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
 }
