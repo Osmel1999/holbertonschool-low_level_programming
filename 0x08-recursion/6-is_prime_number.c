137a49e75e5ba66e42ed78fc61ed4c10f2aca14b
osmel <odfarakm@gmail.com>
2020-02-26 20:28:49 -0500

    New

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 
-int 2_prime(int n, int i);
+int actual_prime(int n, int i);
 
 /**
  * is_prime_number - says if an integer is a prime number or not
@@ -10,23 +10,23 @@ int 2_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
-if (n <= 1)
-return (0);
-return (2_prime(n, n - 1));
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
 }
 
 /**
- * 2_prime - calculates if a number is prime recursively
+ * actual_prime - calculates if a number is prime recursively
  * @n: number to evaluate
  * @i: iterator
  *
  * Return: 1 if n is prime, 0 if not
  */
-int 2_prime(int n, int i)
+int actual_prime(int n, int i)
 {
-if (i == 1)
-return (1);
-if (n % i == 0 && i > 0)
-return (0);
-return (2_prime(n, i - 1));
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
 }
