91280fce8cdbf41f7bb75b90c3c9a08a44ee006f
osmel <odfarakm@gmail.com>
2020-02-26 20:03:57 -0500

    new

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * _strlen_recursion - check the code for Holberton School students.
+ * @s: pointer
  * Return: Always 0.
  */
 int _strlen_recursion(char *s)
