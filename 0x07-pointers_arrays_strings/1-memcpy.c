41d7c8319fd18a475327835f2f5faacb335fc398
osmel <odfarakm@gmail.com>
2020-02-26 20:34:45 -0500

    fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,21 @@
 #include "holberton.h"
+
 /**
- * *_memcpy - check the code for Holberton School students.
- * @dest: pointer
- * @src: pointer
- * @n: variable
- * Return: pointer dest
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int a;
-for (a = 0; a < n; a++)
-{
-dest[a] = src[a];
-}
-return (dest);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
 }
