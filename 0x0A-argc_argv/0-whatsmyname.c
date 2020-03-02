af957072e484435ba828d022a85aea1cdb4d42a0
osmel <odfarakm@gmail.com>
2020-03-01 20:27:47 -0500

    new

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - function
+ * @argc: number of function
+ * @argv: array of funtion
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
