fde26194d406bb177a85d6e450404555bfc9723e
osmel <odfarakm@gmail.com>
2020-03-01 20:39:10 -0500

    New

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - function
+ * @argc: variable
+ * @argv: array 
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
