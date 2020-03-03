da6873416a81ce597c89646717d0f8bbdbd74174
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-03 11:22:09 -0500

    New

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include  "stdlib.h"
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *a;
+int i;
+
+a = malloc((len + 1) * sizeof(char));
+if (s == NULL)
+{
+return (NULL);
+}
+i = 0;
+while (i < len)
+{
+s[i] = c;
+i++
+}
+s[i] = '\0';
+return (s);
+}
