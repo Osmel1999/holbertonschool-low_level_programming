0e44969f955a58b9ff125fd7c76a8d2765fa90ab
osmel <odfarakm@gmail.com>
2020-03-05 22:30:22 -0500

    new

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- * malloc_checked - Allocates memory
- * @b: Numbers of bytes
- * Return: Pointer to the allocated memory
+ * malloc_checked - check the code for Holberton School students.
+ * @b: variable
+ * Return: Always 0.
  **/
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *i;
 
 	ptr = malloc(b);
-	if (ptr == NULL)
+	if (i == NULL)
 		exit(98);
 
-	return (ptr);
+	return (i);
 }
 
