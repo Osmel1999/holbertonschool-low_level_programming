8bbc14d797697809b449153294ee6461d96ecf3e
osmel <odfarakm@gmail.com>
2020-03-15 18:38:27 -0500

    new

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,30 @@
-# ifndef VAR_FUNC_H
-# define  VAR_FUNC_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
-# include  < stdio.h >
-# include  < stdarg.h >
+#include <stdarg.h>
+#include <stdio.h>
 
-int  sum_them_all ( const  unsigned  int n, ...);
-void  print_numbers ( const  char * separator, const  unsigned  int n, ...);
-void  print_strings ( const  char * separator, const  unsigned  int n, ...);
-void  print_all ( const  char * const format, ...);
+int sum_them_all(const unsigned int, ...);
+void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
+void print_all(const char * const, ...);
 
-# endif
+void print_string(va_list);
+void print_char(va_list);
+void print_integer(va_list);
+void print_float(va_list);
+
+
+/**
+ * struct opt - Typedef struct
+ *
+ * @op: Format
+ * @f: The function associated
+ **/
+typedef struct opt
+{
+	char *op;
+	void (*f)(va_list);
+} p_opt;
+
+#endif
